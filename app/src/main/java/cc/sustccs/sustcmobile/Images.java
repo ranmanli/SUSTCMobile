9f896e91161826980c48e47150358d10b4ccb040
ranmanli <ranmanli@outlook.com>
2017-05-27 01:31:26 +0800

    first commit

---
+++ app/src/main/java/cc/sustccs/sustcmobile/Images.java
@@ -0,0 +1,12 @@
+package cc.sustccs.sustcmobile;
+
+/**
+ * Created by ranmanli on 2017/5/25.
+ */
+
+public class Images {
+
+    public final static String[] imageUrls = new String[] {
+            "http://img.my.csdn.net/uploads/201309/01/1378037235_3453.jpg",
+            "http://img.my.csdn.net/uploads/201308/31/1377949442_4562.jpg" };
+}
